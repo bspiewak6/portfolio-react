a102142f27a93d38991781aeab110695a7c5b050
Spiewak <bspiewak6@gmail.com>
2021-01-02 11:55:26 -0500

    updated footer color scheme and contact form section margins

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,13 +7,13 @@ function Footer() {
         <footer className="d-flex justify-content-center footer">
             <div className='flex-row'>
                 <div className='px-2'>
-                    <a href="https://github.com/bspiewak6" target="_blank" rel='noreferrer'><i className="fab fa-github fa-3x"></i></a>
+                    <a className="link" href="https://github.com/bspiewak6" target="_blank" rel='noreferrer'><i className="fab fa-github fa-3x"></i></a>
                 </div>
                 <div className='px-2'>
-                    <a href="https://www.linkedin.com/in/brianspiewak/" target="_blank" rel='noreferrer'><i className="fab fa-linkedin fa-3x"></i></a>
+                    <a className="link" href="https://www.linkedin.com/in/brianspiewak/" target="_blank" rel='noreferrer'><i className="fab fa-linkedin fa-3x"></i></a>
                 </div>
                 <div className='px-2'>
-                    <a href={`${Resume}`} download="Resume"><i class="fas fa-file-download fa-3x"></i></a>
+                    <a className="link" href={`${Resume}`} download="Resume"><i class="fas fa-file-download fa-3x"></i></a>
                 </div>
             </div>
         </footer>
