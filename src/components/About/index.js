1f77a63333f7db81387b7e9749687b292287d590
Spiewak <bspiewak6@gmail.com>
2021-01-02 12:37:16 -0500

    about section styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,19 +3,20 @@ import React from 'react';
 function About() {
 
     return(
-        <section>
-            <h1 class="title">
+        <section className="about-container">
+            <h1 className="about-title">
                 My Story
             </h1>
                 <div>
-                    <p class="about-me">
+                    <p className="about-me-content">
                         I am a results-driven marketing director with 10-plus years’ experience of 
                         innovation, branding, communications and outreach. I have a strong track 
                         record driving revenue through the management of cross-functional creative, 
                         research and operational teams. In addition, I believe that my determination, 
                         organization and will power allows me to work in fast paced environments. My 
                         career up to this point has always provided challenging tasks, but it has been 
-                        a joy at the same time. I’ve worked with very talented people and it’s always pushed me to become better at my craft.  
+                        a joy at the same time. I’ve worked with very talented people and it’s always 
+                        pushed me to become better at my craft.  
                     </p>
                 </div>
         </section>
