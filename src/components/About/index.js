7b618867d58f41ecdb8a681db0af53557b357916
Spiewak <bspiewak6@gmail.com>
2021-01-06 16:36:12 -0500

    formatted About page and updated Project component to include array of projects for future proofing

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,46 +10,37 @@ function About() {
             </h1>
 
             <div className="card about-card">
-                <div className="card-horizontal">
-                    <div className="img-square-wrapper">
+                {/* <div className="card-horizontal"> */}
+                    <div className="card-body">
                         <img 
                             src={headshot}
                             alt={(`${headshot} of developer`)}
-                            className="about-headshot"
+                            className="img-fluid img-responsive"
                         />
-                    </div>
-                    
-                    <div className="card-body">
                         <p className="card-text about-me-content">
-                        Let me take a minute, just sit right there so I can tell you about myself. 
-                        Born and raised in Philly, I moved to NYC 10+ years ago and haven't looked back. 
-                        Aside, from my sports allegiances which will never leave Broad Street.
-                        My career thus far has been in the finance world working for an asset management company 
-                        where I was at the helm of 10-person marketing department. My team was the centralized agency 
-                        at the heart of this publicly traded company that sold securities and built portfolios in many 
-                        different real estate sectors. The company also owned subsidiaries in technology, research and 
-                        financial advice. My team worked tirelessly on the innovation, branding, communications and 
-                        outreach for the entire company and its six business lines.
+                        Born and raised in Philly, I moved to New York City 11 years ago and haven't looked back, aside from my sports allegiances 
+                        which will never leave Broad Street. My career thus far has been in the finance world working for an asset management company 
+                        where I was at the helm of a 10- person marketing department. My team was the centralized agency at the heart of this publicly 
+                        traded company that sold securities and built portfolios in many different real estate sectors. The company also had subsidiaries
+                        in technology, research and financial advice. My team worked tirelessly on the innovation, branding, communications and outreach 
+                        for the entire company and its six business lines.
                         <br /> 
                         <br />
-                        I am a go-getter and very ambitious so I believe that with my determination, organization 
-                        and will power allows me to work in fast paced environments. My career up to this point has 
-                        always provided challenging tasks, but it has been a joy at the same time. I’ve worked with 
-                        very talented people and it’s always pushed me to become better at my craft.
+                        I am a go-getter and very ambitious, therefore my determination and organization allows me to work in fast paced environments. 
+                        My career up to this point has always provided challenging tasks, but it has been a joy at the same time. I’ve worked with very
+                        talented people and it’s always pushed me to become better at my craft.
                         <br />
                         <br />
-                        Just last year, I took on a new challenge in the form of something I've always been interested in, coding. 
-                        For the last six months, I've been a student at the UPenn Coding Bootcamp. This has given me a 
-                        better understanding of code including many front-end and back-end technologies. Learning to code and
-                        putting these skills to use has been very rewarding because I am someone who likes to see things from
-                        start to finish. Projects get me motivated and learning something new is always fascinating to me. The next
-                        chapter of my career is yet to be written, but I am eager to keep moving onward and upward!
-                        
+                        Just last year, I took on a new challenge in the form of something I've always been interested in, web development. For the last 
+                        six months, I've been a student at the UPenn Coding Bootcamp. This has given me a better understanding of both front-end and back-end 
+                        technologies. Learning to code and putting these skills to use has been very rewarding because I am someone who likes to see my work 
+                        come to life. Projects get me motivated and learning something new is always fascinating. The next chapter of my career is yet to be 
+                        written, but I am eager to keep moving onward and upward!
                         </p>
                     </div>
                 </div>
         
-            </div>
+            {/* </div> */}
         </section>
     );
 }
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -6,6 +6,58 @@ import project4 from '../../assets/images/socialnetwork.png';
 import project5 from '../../assets/images/devblog.png';
 import project6 from '../../assets/images/weatherdashboard.png';
 
+const projects = [
+    {
+        id: '1',
+        name: 'Simply ReUse',
+        technologies: ['Node.js, Express.js, Handlebars.js, MySQL and the Sequelize ORM, HTML, CSS, JavaScript, MVC Framework, fuse.js, Heroku'],
+        url: 'https://simplyreuse3.herokuapp.com/',
+        repo: 'Simply-ReUse',
+        image: project1
+    },
+    {
+        id: '2',
+        name: 'TuneOut',
+        technologies: ['HTML, JavaScript, Materialize CSS, Moment.js, TasteDive Music API, NY Times API'],
+        url: 'https://bspiewak6.github.io/music/',
+        repo: 'music',
+        image: project2
+    },
+    {
+        id: '3',
+        name: 'Budget Tracker',
+        technologies: ['JavaScript, Express.js, MongoDB, MongoDB Atlas, Mongoose.js, Heroku'],
+        url: 'https://budgettrackerbds.herokuapp.com/',
+        repo: 'budget-tracker',
+        image: project3
+    },
+    {
+        id: '4',
+        name: 'Social Network CLI',
+        technologies: ['JavaScript, Node.js, Express.js API, MongoDB, Mongoose.js, Moment.js'],
+        url: 'https://drive.google.com/file/d/1N7_QbDM4W843a8pjSLmjCFwOU3FIGJo0/view',
+        repo: 'social-network',
+        image: project4
+    },
+    {
+        id: '5',
+        name: 'Developers Everywhere',
+        technologies: ['JavaScript, Node.js, Express.js API, Sequelize, MVC Framework, Handlebars.js, MySQL2'],
+        url: 'https://sleepy-stream-52263.herokuapp.com/',
+        repo: 'developer-techblog',
+        image: project5
+    },
+    {
+        id: '6',
+        name: 'Weather Dashboard',
+        technologies: ['HTML, CSS, JavaScript, jQuery, Bootstrap, Moment.js, OpenWeather API'],
+        url: 'https://bspiewak6.github.io/weather-dashboard/',
+        repo: 'weather-dashboard',
+        image: project6
+    },
+
+]
+
 const Project = () => {
     return (
         <section className="project-container">
@@ -18,65 +70,21 @@ const Project = () => {
             </h5>
 
             <div className="card-columns">
-                <div className="card">
-                    <div className="card-image">
-                        <img className="card-img-top" src={project1} alt="Simply ReUse App" />
-                        <div className="image-overlay">
-                        <h4 className="card-title"><a href="https://simplyreuse3.herokuapp.com/" target="_blank" rel="noreferrer">Simply ReUse</a></h4>
-                        <p className="text-muted"><a href="https://github.com/bspiewak6/Simply-ReUse" target="_blank" rel="noreferrer">GitHub Repository</a></p>
+                {projects.map((project) => (
+                    <div key={project.repo} className='card'>
+                        <div className="card-image">
+                            <img
+                                src={project.image}
+                                alt={(`${project.name} App`)}
+                                className='card-img-top'
+                            />
+                            <div className="image-overlay">
+                                <h4 className="card-title"><a href={project.url} target='_blank' rel="noreferrer">{project.name}</a></h4>
+                                <p className="text-muted"><a href={(`https://github.com/bspiewak6/${project.repo}`)} target='_blank' rel="noreferrer">GitHub Repository</a></p>
+                            </div>
                         </div>
                     </div>
-                </div>
-
-                <div className="card">
-                    <div className="card-image">
-                        <img className="card-img-top" src={project4} alt="Social Network CLI" />
-                        <div className="image-overlay">
-                            <h4 className="card-title">Social Network CLI</h4>
-                            <p className="text-muted"><a href="https://github.com/bspiewak6/social-network" target="_blank" rel="noreferrer">GitHub Repository</a></p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <div className="card-image">
-                        <img className="card-img-top" src={project2} alt="TuneOut" />
-                        <div className="image-overlay">
-                            <h4 className="card-title"><a href="https://bspiewak6.github.io/music/" target="_blank" rel="noreferrer">TuneOut Entertainment</a></h4>
-                            <p className="text-muted"><a href="https://github.com/bspiewak6/music" target="_blank" rel="noreferrer">GitHub Repository</a></p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <div className="card-image">
-                        <img className="card-img-top" src={project5} alt="Dev Blog" />
-                        <div className="image-overlay">
-                            <h4 className="card-title"><a href="https://sleepy-stream-52263.herokuapp.com/" target="_blank" rel="noreferrer">Developers Everywhere</a></h4>
-                            <p className="text-muted"><a href="https://github.com/bspiewak6/developer-techblog" target="_blank" rel="noreferrer">GitHub Repository</a></p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <div className="card-image">
-                        <img className="card-img-top" src={project3} alt="Budget Tracker" />
-                        <div className="image-overlay">
-                            <h4 className="card-title"><a href="https://budgettrackerbds.herokuapp.com/" target="_blank" rel="noreferrer">Budget Tracker PWA</a></h4>
-                            <p className="text-muted"><a href="https://github.com/bspiewak6/budget-tracker" target="_blank" rel="noreferrer">GitHub Repository</a></p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <div className="card-image">
-                        <img className="card-img-top" src={project6} alt="Weather Dashboard" />
-                        <div className="image-overlay">
-                            <h4 className="card-title"><a href="https://bspiewak6.github.io/weather-dashboard/" target="_blank" rel="noreferrer">Weather Dashboard</a></h4>
-                            <p className="text-muted"><a href="https://github.com/bspiewak6/weather-dashboard" target="_blank" rel="noreferrer">GitHub Repository</a></p>
-                        </div>
-                    </div>
-                </div>
+                ))}
             </div>
         </section>
     );
@@ -85,71 +93,80 @@ const Project = () => {
 export default Project;
 
 
-// const projects = [
-//     {
-//         id: '1',
-//         name: 'Simply ReUse',
-//         technologies: ['Node.js, Express.js, Handlebars.js, MySQL and the Sequelize ORM, HTML, CSS, JavaScript, MVC Framework, fuse.js, Heroku'],
-//         url1: 'https://simplyreuse3.herokuapp.com/',
-//         repo1: 'Simply-ReUse',
-//         image: project1
-//     },
-//     {
-//         id: '2',
-//         name: 'TuneOut',
-//         technologies: ['HTML, JavaScript, Materialize CSS, Moment.js, TasteDive Music API, NY Times API'],
-//         url: 'https://bspiewak6.github.io/music/',
-//         repo: 'music',
-//         image: project2
-//     },
-//     {
-//         id: '3',
-//         name: 'Budget Tracker',
-//         technologies: ['JavaScript, Express.js, MongoDB, MongoDB Atlas, Mongoose.js, Heroku'],
-//         url: 'https://budgettrackerbds.herokuapp.com/',
-//         repo: 'budget-tracker',
-//         image: project3
-//     },
-//     {
-//         id: '4',
-//         name: 'Social Network CLI',
-//         technologies: ['JavaScript, Node.js, Express.js API, MongoDB, Mongoose.js, Moment.js'],
-//         url: 'https://bspiewak6.github.io/social-network/',
-//         repo: 'social-network',
-//         image: project4
-//     },
-//     {
-//         id: '5',
-//         name: 'Developers Everywhere',
-//         technologies: ['JavaScript, Node.js, Express.js API, Sequelize, MVC Framework, Handlebars.js, MySQL2'],
-//         url: 'https://bspiewak6.github.io/developer-techblog/',
-//         repo: 'developer-techblog',
-//         image: project5
-//     },
-//     {
-//         id: '6',
-//         name: 'Weather Dashboard',
-//         technologies: ['HTML, CSS, JavaScript, jQuery, Bootstrap, Moment.js, OpenWeather API'],
-//         url: 'https://bspiewak6.github.io/developer-techblog/',
-//         repo: 'weather-dashboard',
-//         image: project6
-//     },
+/* <div className="card">
+    <div className="card-image">
+        <img className="card-img-top" src={project1} alt="Simply ReUse App" />
+        <div className="image-overlay">
+        <h4 className="card-title"><a href="https://simplyreuse3.herokuapp.com/" target="_blank" rel="noreferrer">Simply ReUse</a></h4>
+        <p className="text-muted"><a href="https://github.com/bspiewak6/Simply-ReUse" target="_blank" rel="noreferrer">GitHub Repository</a></p>
+        </div>
+    </div>
+</div> */
 
-// ]
+/* <div className="card">
+    <div className="card-image">
+        <img className="card-img-top" src={project4} alt="Social Network CLI" />
+        <div className="image-overlay">
+            <h4 className="card-title">Social Network CLI</h4>
+            <p className="text-muted"><a href="https://github.com/bspiewak6/social-network" target="_blank" rel="noreferrer">GitHub Repository</a></p>
+        </div>
+    </div>
+</div>
 
-// <div className='project-grid'>
-//     {projects.map((project) => ( 
-//         <div key={project.repo} className='grid-item'>
-//             <img 
-//                 src={project.image}
-//                 alt={(`${project.name} App`)}
-//                 className='project-img'
-//             />
-//             <a href={project.url} target='_blank' rel="noreferrer" >
-//                 <h3 className='project-name'>{project.name}</h3>
-//             </a>
-//             <a href={(`https://github.com/bspiewak6/${project.repo}`)} target='_blank' rel="noreferrer" className='project-feature'>GitHub Repository</a>
-//         </div>  */}
-//     ))}
-// </div>
+<div className="card">
+    <div className="card-image">
+        <img className="card-img-top" src={project2} alt="TuneOut" />
+        <div className="image-overlay">
+            <h4 className="card-title"><a href="https://bspiewak6.github.io/music/" target="_blank" rel="noreferrer">TuneOut Entertainment</a></h4>
+            <p className="text-muted"><a href="https://github.com/bspiewak6/music" target="_blank" rel="noreferrer">GitHub Repository</a></p>
+        </div>
+    </div>
+</div>
+
+<div className="card">
+    <div className="card-image">
+        <img className="card-img-top" src={project5} alt="Dev Blog" />
+        <div className="image-overlay">
+            <h4 className="card-title"><a href="https://sleepy-stream-52263.herokuapp.com/" target="_blank" rel="noreferrer">Developers Everywhere</a></h4>
+            <p className="text-muted"><a href="https://github.com/bspiewak6/developer-techblog" target="_blank" rel="noreferrer">GitHub Repository</a></p>
+        </div>
+    </div>
+</div>
+
+<div className="card">
+    <div className="card-image">
+        <img className="card-img-top" src={project3} alt="Budget Tracker" />
+        <div className="image-overlay">
+            <h4 className="card-title"><a href="https://budgettrackerbds.herokuapp.com/" target="_blank" rel="noreferrer">Budget Tracker PWA</a></h4>
+            <p className="text-muted"><a href="https://github.com/bspiewak6/budget-tracker" target="_blank" rel="noreferrer">GitHub Repository</a></p>
+        </div>
+    </div>
+</div>
+
+<div className="card">
+    <div className="card-image">
+        <img className="card-img-top" src={project6} alt="Weather Dashboard" />
+        <div className="image-overlay">
+            <h4 className="card-title"><a href="https://bspiewak6.github.io/weather-dashboard/" target="_blank" rel="noreferrer">Weather Dashboard</a></h4>
+            <p className="text-muted"><a href="https://github.com/bspiewak6/weather-dashboard" target="_blank" rel="noreferrer">GitHub Repository</a></p>
+        </div>
+    </div>
+</div> */
+
+
+{/* <div className='project-grid'>
+    {projects.map((project) => (
+        <div key={project.repo} className='grid-item'>
+            <img
+                src={project.image}
+                alt={(`${project.name} App`)}
+                className='project-img'
+            />
+            <a href={project.url} target='_blank' rel="noreferrer" >
+                <h3 className='project-name'>{project.name}</h3>
+            </a>
+            <a href={(`https://github.com/bspiewak6/${project.repo}`)} target='_blank' rel="noreferrer" className='project-feature'>GitHub Repository</a>
+        </div>
+    ))}
+</div> */}
 
